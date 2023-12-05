6dc921dbb464ef7257453c88676879e7de299c3e
sh1nheewon <tlsgmldnjs00@naver.com>
2023-12-05 16:41:19 +0900

    BlackJack

---
+++ Project_001/src/BlackJack/BlackJack.java
@@ -0,0 +1,15 @@
+package BlackJack;
+
+import java.util.Scanner;
+
+public class BlackJack {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		while(true){
+			for(int i = 0 ; i < 13 ; i++) {
+				
+			}
+		}
+	}
+}
--- Project_001/src/project_01/Project1.java
+++
@@ -1,7 +0,0 @@
-package project_01;
-
-public class Project1 {
-	public static void main(String[] args) {
-		
-	}
-}
