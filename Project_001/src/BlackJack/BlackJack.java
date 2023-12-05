ab96fb47c32d316453b462a655a0463d3d517639
sh1nheewon <tlsgmldnjs00@naver.com>
2023-12-05 21:44:34 +0900

    project

--- Project_001/src/BlackJack/BlackJack.java
+++ Project_001/src/BlackJack/BlackJack.java
@@ -1,15 +1,38 @@
 package BlackJack;
 
+import java.util.List;
 import java.util.Scanner;
 
+import BlackJack.Blackjack_model.CardsDto;
+
 public class BlackJack {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	private Scanner scan;
+	private List<CardsDto> cards = null;	
+	
+public void loadCards() {
 		
-		while(true){
-			for(int i = 0 ; i < 13 ; i++) {
+		while(scan.hasNext()) {
+			String line = scan.nextLine();
+			CardsDto cardsDto = new CardsDto();
+			
+			for(int i = 2 ; i < 10 ; i++) {
+				String[] lines = new String[i];
+			
+			try {
+				cardsDto.Diamonds = lines[0];
+				cardsDto.Hearts = lines[1];
+				cardsDto.Spades = lines[2];
+				cardsDto.Clubs = lines[3];
 				
+			}catch (Exception e) {
+				System.out.println("데이터 읽는 중 오류 발생");
+				System.out.println(lines);
+				break;
 			}
-		}
-	}
+			cards.add(cardsDto);
+			System.out.println(lines[i]);
+			}
+			}
+}		
 }
+
---
+++ Project_001/src/BlackJack/Blackjack_model.java
@@ -0,0 +1,12 @@
+package BlackJack;
+
+public class Blackjack_model {
+	public class CardsDto{
+		
+		public String Diamonds;
+		public String Hearts;
+		public String Spades;
+		public String Clubs;
+		
+	}
+}
