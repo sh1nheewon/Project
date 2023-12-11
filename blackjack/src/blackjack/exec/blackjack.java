9925d7577babbeb94bafb37dd4e1b68f2b6c048c
sh1nheewon <tlsgmldnjs00@naver.com>
2023-12-12 08:57:04 +0900

    blackjack

--- blackjack/src/blackjack/exec/blackjack.java
+++ blackjack/src/blackjack/exec/blackjack.java
@@ -5,7 +5,7 @@ import blackjack.service.bService_Player;
 public class blackjack {
 	public static void main(String[] args) {
 		bService_Player playerService = new bService_Player();
-		
+		playerService.start();
 		playerService.inputBlackJack();
 		playerService.printBlackJack();
 	
